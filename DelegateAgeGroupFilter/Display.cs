574e65a148a6af5d863d7ebfb1a3fc76ae7d0e47
Richard Chalk <barmybrit@gmail.com>
2023-07-06 14:27:29 +0200

    Add project files.

---
+++ DelegateAgeGroupFilter/Display.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateAgeGroupFilter
+{
+    public static class Display
+    {
+        public static void DisplayPeople(string title, List<Person> people, FilterDelegate filter)
+        {
+            Console.WriteLine(title);
+            for (int i = 0; i < title.Length; i++)
+            {
+                Console.Write("=");
+            }
+            
+            Console.WriteLine();
+
+            foreach (var p in people)
+            {
+                if (filter(p))
+                {
+                    Console.WriteLine($"{p.Name} is {p.Age} years old");
+                }
+            }
+        }
+
+        // Filter methods
+        public static bool IsChild(Person p)
+        {
+            return p.Age < 18; 
+        }
+
+        public static bool IsAdult(Person p)
+        {
+            return p.Age >= 18;
+        }
+
+        public static bool IsPensioner(Person p)
+        {
+            return p.Age >= 65;
+        }
+    }
+}
---
+++ DelegateAgeGroupFilter/Program.cs
@@ -0,0 +1,21 @@
+﻿
+namespace DelegateAgeGroupFilter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var p1 = new Person() { Name = "Richard", Age= 53 };
+            var p2 = new Person() { Name = "Stefan", Age= 54 };
+            var p3 = new Person() { Name = "Lucas", Age= 9 };
+            var p4 = new Person() { Name = "Alicia", Age= 16 };
+            var p5 = new Person() { Name = "Gun", Age= 80 };
+
+            var people = new List<Person>() { p1, p2, p3, p4, p5 };
+                        
+            Display.DisplayPeople("Barn", people, Display.IsChild);
+            Display.DisplayPeople("Vuxna", people, Display.IsAdult);
+            Display.DisplayPeople("Oldies", people, Display.IsPensioner);
+        }
+    }
+}
